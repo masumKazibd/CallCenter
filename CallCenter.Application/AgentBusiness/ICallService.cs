96c021b83de7c9ad6564b0890ca297ce2673bde6
masumkazibd <masum.kazi.hantmk@gmail.com>
2026-07-24 10:16:13 +0600

    interfaces

---
+++ CallCenter.Application/AgentBusiness/ICallService.cs
@@ -0,0 +1,16 @@
+﻿using CallCenter.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallCenter.Application.AgentBusiness
+{
+    public interface ICallService
+    {
+        Task<IEnumerable<CallDTO>> GetAllAsync();
+        Task<CallDTO?> GetByIdAsync(int id);
+        Task<CallDTO> CreateAsync(CallDTO dto);
+        Task<bool> UpdateStatusAsync(int id, string status);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ CallCenter.Application/AgentBusiness/IQueueService.cs
@@ -0,0 +1,16 @@
+﻿using CallCenter.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallCenter.Application.AgentBusiness
+{
+    public interface IQueueService
+    {
+        Task<IEnumerable<QueueDTO>> GetAllAsync();
+        Task<QueueDTO?> GetByIdAsync(int id);
+        Task<QueueDTO> CreateAsync(QueueDTO dto);
+        Task<bool> UpdateAsync(int id, QueueDTO dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
