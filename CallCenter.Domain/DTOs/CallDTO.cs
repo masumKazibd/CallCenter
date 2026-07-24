48dd66360c5c1fb59102c563c42c7599e9224859
masumkazibd <masum.kazi.hantmk@gmail.com>
2026-07-24 10:12:19 +0600

    created teh dtos

---
+++ CallCenter.Domain/DTOs/CallDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallCenter.Domain.DTOs
+{
+    public class CallDTO
+    {
+        public int Id { get; set; }
+        public string Direction { get; set; }   // "Inbound" or "Outbound"
+        public string FromNumber { get; set; }
+        public string ToNumber { get; set; }
+        public string Status { get; set; }       // set by server
+        public int? AgentId { get; set; }
+        public int? QueueId { get; set; }
+        public DateTime StartedAt { get; set; }
+        public DateTime? AnsweredAt { get; set; }
+        public DateTime? EndedAt { get; set; }
+        public int DurationSeconds { get; set; }
+        public string? RecordingUrl { get; set; }
+        public string? CrmCustomerId { get; set; }
+    }
+}
