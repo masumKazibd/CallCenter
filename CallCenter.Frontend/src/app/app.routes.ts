958e870c8e647134127217353a7244d6428dcac4
masumkazibd <masum.kazi.hantmk@gmail.com>
2026-07-26 23:51:16 +0600

    added in route

--- CallCenter.Frontend/src/app/app.routes.ts
+++ CallCenter.Frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard.component/dashboard.component';
 import { AgentComponent } from './components/agent.component/agent.component';
+import { CallSimulatorComponent } from './components/call-simulator.component/call-simulator.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'agent', component: AgentComponent },
+  { path: 'call-simulator', component: CallSimulatorComponent }
 ];
