106ac2b742829ecd932b072e781ce95738b81b1c
masumkazibd <masum.kazi.hantmk@gmail.com>
2026-07-26 22:55:11 +0600

    using signalR for simulating the call

---
+++ CallCenter.Api/CallHub/CallHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CallCenter.Api.CallHub
+{
+    public class CallHub : Hub
+    { 
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"✅ Client Connected: {Context.ConnectionId}");
+            await base.OnConnectedAsync();
+        }
+         
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            Console.WriteLine($"❌ Client Disconnected: {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
